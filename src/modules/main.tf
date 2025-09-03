b39e420337b228c5a29bb03fa70539e5716177b1
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-03 22:55:17 +0530

    Update main.tf

--- src/modules/main.tf
+++ src/modules/main.tf
@@ -16,3 +16,16 @@ resource "azurerm_resource_group" "RG1" {
   name     = "terraformPractice"
   location = "EAST US"
 }
+
+
+resource "azurerm_storage_account" "SA" {
+  name                     = "machakishan"
+  resource_group_name      = azurerm_resource_group.RG1.name
+  location                 = azurerm_resource_group.RG1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Dev"
+  }
+}
