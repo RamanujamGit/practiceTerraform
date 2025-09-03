257eb682ac1b6931be807f4b2aaff53d13edcffb
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-04 00:31:01 +0530

    Update main.tf

--- src/modules/main.tf
+++ src/modules/main.tf
@@ -12,7 +12,23 @@ provider "azurerm" {
 
 }
 
-resource "azurerm_resource_group" "RG1" {
-  name     = "terraformPractice"
-  location = "EAST US"
+terraform {
+  backend "azurerm" {
+    access_key           = "FMp287u24dPtkMtnxUYBLsWVNnTXzzFl9XRB//KTUze/bTr/W4yv2bqU+m/SFauOPgDUAxCqtX4V+ASt326YzQ=="
+    resource_group_name  = "practiceTerraform"
+    storage_account_name = "machakishan"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
+
+data "azurerm_management_group" "rg1" {
+  name = "practiceTerraform"
+}
+
+output "resource_group_name" {
+  value = data.azurerm_management_group.rg1.name
+}
+
+
+
