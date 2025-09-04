dcde8e6053f6e597781723ddbf34fe396291ffe9
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-04 16:43:11 +0530

    Update providers.tf

--- src/providers.tf
+++ src/providers.tf
@@ -11,3 +11,13 @@ provider "azurerm" {
   features {}
 
 }
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "practiceTerraform"
+    storage_account_name = "machakishan"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
