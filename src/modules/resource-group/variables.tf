b2fef511e2e1b9128aa5eb416a9927c784070f19
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-04 17:44:30 +0530

    Create variables.tf

---
+++ src/modules/resource-group/variables.tf
@@ -0,0 +1,10 @@
+
+variable "rg_name" {
+  type        = string
+  description = "name of the resource group"
+}
+
+variable "location" {
+  type         = string
+  description  = "Enter the location"
+}
