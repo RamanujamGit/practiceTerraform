604604ae515d68fe415209d381b2e9d0f3e64c80
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-04 17:21:03 +0530

    Update main.tf

--- src/modules/resource-group/main.tf
+++ src/modules/resource-group/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "RG2" {
-  name     = name_rg
-  location = var.locations
+  name     = "${var.name_rg}"
+  location = "${var.locations}"
   tags = {
     environment = "dev"
   }
