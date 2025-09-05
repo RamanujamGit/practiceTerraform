0d057ba8f5961ae003356e7802439ab8edcd6e0c
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-05 13:48:36 +0530

    plz p and c

--- src/modules/Virtual-network/variables.tf
+++ src/modules/Virtual-network/variables.tf
@@ -1,12 +1,4 @@
-variable "rg_name" {
-    type   =   string
-    description = "This is the name of resource group"
-}
 
-variable "location" {
-    type   =  string
-    description = "location of resource group"
-}
 
 variable "vnetName" {
     type  = string
---
+++ src/modules/resource-group/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+    type   =   string
+    description = "This is the name of resource group"
+}
+
+variable "location" {
+    type   =  string
+    description = "location of resource group"
+}
