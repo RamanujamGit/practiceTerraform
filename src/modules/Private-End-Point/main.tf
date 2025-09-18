5f557a339f3ac8f2678cfeca595f5f918185bc93
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-18 14:53:52 +0530

    16 commit

--- src/modules/Private-End-Point/main.tf
+++ src/modules/Private-End-Point/main.tf
@@ -10,14 +10,10 @@ resource "azurerm_private_endpoint" "eventhub_pe" {
     subresource_names              = ["namespace"]
     is_manual_connection           = false
   }
-}
 
-resource "azurerm_private_dns_zone_group" "eventhub_dns_group" {
-  name                 = "eventhub-dns-group"
-  private_endpoint_id  = azurerm_private_endpoint.eventhub_pe.id
-
-  private_dns_zone_configs {
-    name                  = "eventhub-dns-zone"
-    private_dns_zone_id   = var.prDNSZoneID  # Pass your DNS zone ID as a variable
+    private_dns_zone_group {
+    name                 = "example-dns-zone-group"
+    private_dns_zone_ids = var.prDNSZoneID
   }
 }
+
