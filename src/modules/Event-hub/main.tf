62c46fcb66879cc6d0c32b286139f579026805e3
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-24 18:23:45 +0530

    commiting

--- src/modules/Event-hub/main.tf
+++ src/modules/Event-hub/main.tf
@@ -13,18 +13,23 @@ resource "azurerm_eventhub_namespace" "EvntHubNS" {
 }
 
 resource "azurerm_eventhub" "EvntHub" {
-  for_each = {
-    for ns in var.namespaces :
-    for eh in ns.eventhubs :
-    "${ns.name}/${eh.name}" => {
-      namespace_id        = azurerm_eventhub_namespace.EvntHubNS[ns.name].id
-      name               = eh.name
-      partition_count    = eh.partition_count
-      message_retention  = eh.message_retention
-      resource_group_name = ns.resource_group_name
-      namespace_name      = ns.name
-    }
-  }
+for_each = {
+  for pair in flatten([
+    for ns in var.namespaces : [
+      for eh in ns.eventhubs : {
+        key = "${ns.name}/${eh.name}"
+        value = {
+          namespace_id        = azurerm_eventhub_namespace.EvntHubNS[ns.name].id
+          name                = eh.name
+          partition_count     = eh.partition_count
+          message_retention   = eh.message_retention
+          resource_group_name = ns.resource_group_name
+          namespace_name      = ns.name
+        }
+      }
+    ]
+  ]) : pair.key => pair.value
+}
   name              = each.value.name
   namespace_id      = each.value.namespace_id
   partition_count   = each.value.partition_count
