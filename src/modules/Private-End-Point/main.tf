6c453860dac24c58832d7e66fef713a03a4ba9e0
RamanujamGit <102577488+RamanujamGit@users.noreply.github.com>
2025-09-24 19:48:17 +0530

    commiting

--- src/modules/Private-End-Point/main.tf
+++ src/modules/Private-End-Point/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_private_endpoint" "eventhub_pe" {
 
   private_service_connection {
     name                           = var.PscName
-    private_connection_resource_id = var.EventhubNSid
+    private_connection_resource_id = var.EventhubNSid[0]
     subresource_names              = ["namespace"]
     is_manual_connection           = false
   }
